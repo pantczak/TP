2e8b15eb1f33f3e0ab8e9d2c05751b3eef62bf14
pantczak <piotrmateuszantczak@protonmail.com>
2020-11-03 15:20:46 +0100

    added unit tests, modified getters and setters, changed ID form int to GUID

--- Task1/Book.cs
+++ Task1/Book.cs
@@ -1,28 +1,31 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     public class Book //KATALAOG w instrukcji
     {
-        private string title;
-        private string author;
-        private string isbn;
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Guid Isbn { get; set; }
 
-        public Book(string title, string author, string isbn)
+        public Book(string title, string author, Guid isbn)
         {
-            this.Title = title;
-            this.Author = author;
-            this.Isbn = isbn;
+            Title = title;
+            Author = author;
+            Isbn = isbn;
         }
-
-        public string Isbn { get => isbn; set => isbn = value; }
-        public string Author { get => author; set => author = value; }
-        public string Title { get => title; set => title = value; }
-
-        public string All { get => Isbn + " " + Title + " " + Author; }
 
         public override bool Equals(object obj)
         {
-            return obj is Book book &&
-                   isbn == book.isbn;
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Book other = (Book)obj;
+                return (this.Isbn.Equals(other.Isbn));
+            }
         }
-    }
+}
 }
--- Task1/DataContext.cs
+++ Task1/DataContext.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Task1
 {
     public class DataContext
     {
-        private List<Reader> readers;
-        private Dictionary<int, Book> books;
-        private ObservableCollection<Event> events;
-        private List<Storage> storages;
+        private List<Client> readers = new List<Client>();
+        private Dictionary<Guid, Book> books = new Dictionary<Guid, Book>();
+        private ObservableCollection<Purchace> purchaces = new ObservableCollection<Purchace>();
+        private ObservableCollection<BookExample> bookExamples = new ObservableCollection<BookExample>();
     }
 }
