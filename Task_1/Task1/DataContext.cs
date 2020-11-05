be589949ab0ba92878974dfa619ad189cf33e16a
pantczak <piotrmateuszantczak@protonmail.com>
2020-11-05 10:22:59 +0100

    Revert "deleted files"
    This reverts commit b25d7814d44044ee11e8e1eec708e262fddeb3d3.

---
+++ Task_1/Task1/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    public class DataContext
+    {
+        private List<Client> readers = new List<Client>();
+        private Dictionary<Guid, Book> books = new Dictionary<Guid, Book>();
+        private ObservableCollection<Purchace> purchaces = new ObservableCollection<Purchace>();
+        private ObservableCollection<BookExample> bookExamples = new ObservableCollection<BookExample>();
+    }
+}
