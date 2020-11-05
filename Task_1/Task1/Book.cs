238034b84989186a9d1f80561b7ab0aa3665b4df
pantczak <piotrmateuszantczak@protonmail.com>
2020-11-05 10:13:15 +0100

    moved files

---
+++ Task_1/Task1/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Book //KATALAOG w instrukcji
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Guid Isbn { get; set; }
+
+        public Book(string title, string author, Guid isbn)
+        {
+            Title = title;
+            Author = author;
+            Isbn = isbn;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Book other = (Book)obj;
+                return (this.Isbn.Equals(other.Isbn));
+            }
+        }
+}
+}
