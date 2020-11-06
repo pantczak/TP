ddc0f4a8b471ce63f04f5aad723d8d51e5f09d87
WitoldPietrzak <witekpietrzak35@gmail.com>
2020-11-06 21:20:13 +0100

    implemented IDataRepository interface methods into DataRepository class

--- Task_1/BookShop/model/IDataRepository.cs
+++ Task_1/BookShop/model/IDataRepository.cs
@@ -1,19 +1,18 @@
 ﻿using BookShop.model.data;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace BookShop.model
 {
     internal interface IDataRepository
     {
         void AddBook(Book book);
-        Book GetBook(int Id);
+        Book GetBook(Guid Id);
         IEnumerable<Book> GetAllBook();
         void UpdateBook(int Id, Book book);
         void DeleteBook(Book book);
         void AddClient(Client client);
-        Client GetClient(Client client);
+        Client GetClient(string PESEL);
         IEnumerable<Client> GetAllClient();
         void UpdateClient(int Id, Client client);
         void DeleteClient(Client client);
