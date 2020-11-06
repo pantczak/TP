3b919d89457b2bc5205c2482710aeef0824eec77
224406 Pietrzak Witold <224406@edu.p.lodz.pl>
2020-11-06 20:43:40 +0100

    Created IDataRepository interface and DataRepository class

---
+++ Task_1/BookShop/model/DataRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.model
+{
+    internal class DataRepository
+    {
+    }
+}
---
+++ Task_1/BookShop/model/IDataRepository.cs
@@ -0,0 +1,21 @@
+﻿using BookShop.model.data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.model
+{
+    internal interface IDataRepository
+    {
+        void AddBook(Book book);
+        Book GetBook(int Id);
+        IEnumerable<Book> GetAllBook();
+        void UpdateBook(int Id, Book book);
+        void DeleteBook(Book book);
+        void AddClient(Client client);
+        Client GetClient(Client client);
+        IEnumerable<Client> GetAllClient();
+        void UpdateClient(int Id, Client client);
+        void DeleteClient(Client client);
+    }
+}
