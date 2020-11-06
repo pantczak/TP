ea13b4152156498595b4aaacb2e84b02138fb4c7
224406 Pietrzak Witold <224406@edu.p.lodz.pl>
2020-11-06 21:20:13 +0100

    implemented IDataRepository interface methods into DataRepository class

--- Task_1/BookShop/model/DataRepository.cs
+++ Task_1/BookShop/model/DataRepository.cs
@@ -1,10 +1,62 @@
-﻿using System;
+﻿using BookShop.model.data;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BookShop.model
 {
-    internal class DataRepository
+    internal class DataRepository : IDataRepository
     {
+        private DataContext data;
+        public void AddBook(Book book)
+        {
+            data.books.Add(book.Isbn,book);
+        }
+
+        public void AddClient(Client client)
+        {
+            data.readers.Add(client);
+        }
+
+        public void DeleteBook(Book book)
+        {
+            data.books.Remove(book.Isbn);
+        }
+
+        public void DeleteClient(Client client)
+        {
+            data.readers.Remove(client);
+        }
+
+        public IEnumerable<Book> GetAllBook()
+        {
+            return (IEnumerable<Book>)data.books;
+        }
+
+        public IEnumerable<Client> GetAllClient()
+        {
+            return data.readers;
+        }
+
+        public Book GetBook(Guid Id)
+        {
+            return data.books[Id];
+        }
+
+        public Client GetClient(string PESEL)
+        {
+            return data.readers.Find(reader => reader.Pesel == PESEL);
+        }
+
+        public void UpdateBook(Book book)
+        {
+            data.books.Add(book.Isbn, book);
+        }
+
+        public void UpdateClient( Client client)
+        {
+            DeleteClient(client);
+            AddClient(client);
+        }
     }
 }
