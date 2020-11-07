6973eef3e33116b1259f5c8b3dc0a080e2458e24
pantczak <piotrmateuszantczak@protonmail.com>
2020-11-07 20:07:25 +0100

    Added and partialy implemented logic layer (DataService)

---
+++ Task_1/BookShop/logic/IDataService.cs
@@ -0,0 +1,20 @@
+﻿using BookShop.model.data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.logic
+{
+    public interface IDataService
+    {
+        void CreateClient(string firstName, string lastName, string pesel);
+        void AddBook(string title, string author, Guid isbn);
+        void AddBookExample(Book book, int tax, double basePrice);
+        void PurchaceBook(Client client, BookExample bookExample);
+        IEnumerable<Client> GetAllClientsByName(string name = null);
+        IEnumerable<Purchace> GetClientPurchaces(Client client);
+        IEnumerable<Purchace> GetPurchacesFromDate(DateTime? start = null, DateTime? end = null);
+        Dictionary<Book, int> GetBooks();
+        IEnumerable<BookExample> GetBookExamples(Guid isbn);
+    }
+}
