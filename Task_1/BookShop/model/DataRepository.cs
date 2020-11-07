156659c1e645a777679eced1e70ed75af7130fb7
pantczak <piotrmateuszantczak@protonmail.com>
2020-11-07 11:47:19 +0100

    Added missing DataRepository methods, changed names in DataContext, made DataFiller  public

--- Task_1/BookShop/model/DataRepository.cs
+++ Task_1/BookShop/model/DataRepository.cs
@@ -1,4 +1,5 @@
 ﻿using BookShop.model.data;
+using BookShop.model.filler;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,56 +8,120 @@ namespace BookShop.model
 {
     public class DataRepository : IDataRepository
     {
-        private DataContext data;
+        private DataContext dataContext;
+
+        public DataRepository(IDataFiller dataFiller)
+        {
+            dataContext = new DataContext();
+            dataFiller.Fill(dataContext);
+        }
+
         public void AddBook(Book book)
         {
-            data.books.Add(book.Isbn,book);
+            if (dataContext.books.ContainsKey(book.Isbn))
+            {
+                throw new Exception("Data already exists");
+            }
+            dataContext.books.Add(book.Isbn, book);
+        }
+
+        public void AddBookExample(BookExample bookExample)
+        {
+            throw new NotImplementedException();
         }
 
         public void AddClient(Client client)
         {
-            data.readers.Add(client);
+            if (dataContext.clients.Contains(client))
+            {
+                throw new Exception("Data already exists");
+            }
+            dataContext.clients.Add(client);
         }
 
-        public void DeleteBook(Book book)
+        public void AddPurchace(Purchace purchace)
         {
-            data.books.Remove(book.Isbn);
+            throw new NotImplementedException();
         }
 
-        public void DeleteClient(Client client)
+        public void DeleteBook(Guid Isbn)
         {
-            data.readers.Remove(client);
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBookExample(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteClient(string Pesel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePurchace(Purchace purchace)
+        {
+            throw new NotImplementedException();
         }
 
         public IEnumerable<Book> GetAllBook()
         {
-            return (IEnumerable<Book>)data.books;
+            return (IEnumerable<Book>)dataContext.books;
+        }
+
+        public IEnumerable<BookExample> GetAllBookExamples()
+        {
+            throw new NotImplementedException();
         }
 
         public IEnumerable<Client> GetAllClient()
         {
-            return data.readers;
+            return dataContext.clients;
+        }
+
+        public IEnumerable<Purchace> GetAllPurchace()
+        {
+            throw new NotImplementedException();
         }
 
         public Book GetBook(Guid Id)
         {
-            return data.books[Id];
+            return dataContext.books[Id];
+        }
+
+        public Book GetBookExample(Guid Id)
+        {
+            throw new NotImplementedException();
         }
 
         public Client GetClient(string PESEL)
         {
-            return data.readers.Find(reader => reader.Pesel == PESEL);
+            return dataContext.clients.Find(reader => reader.Pesel == PESEL);
         }
 
-        public void UpdateBook(Book book)
+        public Purchace GetPurchace(Guid Id)
         {
-            data.books.Add(book.Isbn, book);
+            throw new NotImplementedException();
         }
 
-        public void UpdateClient( Client client)
+        public void UpdateBook(Guid Isbn, Book book)
         {
-            DeleteClient(client);
-            AddClient(client);
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBookExample(Guid Id, BookExample bookExample)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateClient(string PESEL, Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePurchace(Guid id, Purchace purchace)
+        {
+            throw new NotImplementedException();
         }
     }
 }
