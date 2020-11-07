c36d478f58415c3fcae724efe521a133171c4515
pantczak <piotrmateuszantczak@protonmail.com>
2020-11-07 11:16:32 +0100

    Fixed DataContext

--- Task_1/BookShop/model/DataContext.cs
+++ Task_1/BookShop/model/DataContext.cs
@@ -7,9 +7,9 @@ namespace BookShop.model
 {
     internal class DataContext
     {
-        internal List<Client> readers { get; private set; }
-        internal Dictionary<Guid, Book> books { get; private set; }
-        internal ObservableCollection<Purchace> purchaces { get; private set; }
-        internal ObservableCollection<BookExample> bookExamples { get; private set; }
+        internal List<Client> readers { get; private set; } = new List<Client>();
+        internal Dictionary<Guid, Book> books { get; private set; } = new Dictionary<Guid, Book>();
+        internal ObservableCollection<Purchace> purchaces { get; private set; } = new ObservableCollection<Purchace>();
+        internal ObservableCollection<BookExample> bookExamples { get; private set; } = new ObservableCollection<BookExample>();
     }
 }
