6ce06e6769bd744e836839d9bc2f6e44a542dd25
WitoldPietrzak <witekpietrzak35@gmail.com>
2020-11-08 14:36:16 +0100

    Added methods to IDataService interface

--- Task_1/BookShop/logic/IDataService.cs
+++ Task_1/BookShop/logic/IDataService.cs
@@ -11,6 +11,34 @@ namespace BookShop.logic
         void AddBook(string title, string author, Guid isbn);
         void AddBookExample(Book book, int tax, double basePrice);
         void PurchaceBook(Client client, BookExample bookExample);
+        void RemoveClient(Client client);
+        void RemoveBook(Book book);
+        void RemoveBookExample(BookExample bookExample);
+        void RemoveEvent(Event evnt);
+        void ModifyClient(Client oldClient,Client newClient);
+        void ModifyBook(Book oldBook, Book newBook);
+        void ModifyBookExample(BookExample oldBookExample,BookExample newBookExample);
+        void ModifyPurchase(Purchace oldPurchase, Purchace newPurchase);
+        IEnumerable<Book> GetAllBooks();
+        IEnumerable<BookExample> GetAllBookExamples();
+        IEnumerable<Client> GetAllClients();
+        IEnumerable<Event> GetAllEvents();
+        IEnumerable<Purchace> GetAllPurchases();
+        IEnumerable<Purchace> GetPurchasesInDateRange(DateTime from,DateTime to);
+        IEnumerable<Event> GetEventsByClient(Client client);
+        IEnumerable<Event> GetEventsByBookExample(BookExample bookExample);
+        IEnumerable<Book> GetBooksByAuthor(string author);
+        IEnumerable<Book> GetBooksByName(string name);
+        Book GetBookByIsbn(Guid isbn);
+        IEnumerable<BookExample> GetBookExamplesByBook(Book book);
+        IEnumerable<BookExample> GetBookExamplesInPriceRange(double priceMin, double priceMax);
+        IEnumerable<Client> GetClientsByFirstLetterOfName(char letter);
+        IEnumerable<Client> GetClientsByPesel(string pesel);
+
+
+
+
+
 
     }
 }
