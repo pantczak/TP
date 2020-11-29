0c62915eb7dbb298cb2e5231ea2c7ce5596c84f3
pantczak <piotrmateuszantczak@protonmail.com>
2020-11-29 17:46:44 +0100

    Added MySerlializer and JsonSerizlizer

---
+++ Task_2/ConsoleSerializer/Serializer/JsonSerializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ConsoleSerializer.Serializer
+{
+    public class JsonSerializer
+    {
+        private static readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+        {
+            PreserveReferencesHandling = PreserveReferencesHandling.Objects
+        };
+
+        public static void Serialize(Object obj, string filePath)
+        {
+            File.Delete(filePath);
+            using (FileStream stream = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                string serialized =
+                    JsonConvert.SerializeObject(obj, Formatting.Indented, JsonSerializer.JsonSettings);
+                // stream.Write(Encoding.UTF8.GetBytes(serialized));
+                stream.Flush();
+            }
+        }
+    }
+}
---
+++ Task_2/ConsoleSerializer/Serializer/MySerializer.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace ConsoleSerializer.Serializer
+{
+    public class MySerializer : Formatter
+    {
+        public override object Deserialize(Stream serializationStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Serialize(Stream serializationStream, object graph)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ISurrogateSelector SurrogateSelector { get; set; }
+        public override SerializationBinder Binder { get; set; }
+        public override StreamingContext Context { get; set; }
+
+        protected override void WriteArray(object obj, string name, Type memberType)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteBoolean(bool val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteByte(byte val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteChar(char val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteDateTime(DateTime val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteDecimal(decimal val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteDouble(double val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteInt16(short val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteInt32(int val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteInt64(long val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteObjectRef(object obj, string name, Type memberType)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteSByte(sbyte val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteSingle(float val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteTimeSpan(TimeSpan val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteUInt16(ushort val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteUInt32(uint val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteUInt64(ulong val, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void WriteValueType(object obj, string name, Type memberType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
