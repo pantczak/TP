d70f8bb36568c2c03d6c8ba8759eced82521c9db
pantczak <piotrmateuszantczak@protonmail.com>
2020-11-29 18:15:24 +0100

    Added DataModel classes

---
+++ Task_2/ConsoleSerializer/DataModel/Class2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace ConsoleSerializer.DataModel
+{
+    [Serializable]
+    [JsonObject]
+    public class Class2 : ISerializable
+    {
+        public string TextData { get; set; }
+        public DateTime DateTimeData { get; set; }
+        public int IntegerData { get; set; }
+        public Class1 Class1 { get; set; }
+        public Class3 Class3 { get; set; }
+
+        public Class2(string textData, DateTime dateTimeData, int integerData, Class1 class1, Class3 class3)
+        {
+            TextData = textData;
+            DateTimeData = dateTimeData;
+            IntegerData = integerData;
+            Class1 = class1;
+            Class3 = class3;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("TextData", TextData);
+            info.AddValue("DateTimeData", DateTimeData);
+            info.AddValue("IntegerData", IntegerData);
+            info.AddValue("Class1", Class1);
+            info.AddValue("Class3", Class3);
+        }
+    }
+}
