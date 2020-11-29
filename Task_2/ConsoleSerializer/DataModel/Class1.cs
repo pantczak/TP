ba7955d6e487a9511a5e3966051ee15c35f6942e
pantczak <piotrmateuszantczak@protonmail.com>
2020-11-29 18:15:24 +0100

    Added DataModel classes

---
+++ Task_2/ConsoleSerializer/DataModel/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace ConsoleSerializer.DataModel
+{
+    [Serializable]
+    [JsonObject]
+    public class Class1 : ISerializable
+    {
+        public string TextData { get; set; }
+        public DateTime DateTimeData { get; set; }
+        public double DoubleData { get; set; }
+        public Class2 Class2 { get; set; }
+        public Class3 Class3 { get; set; }
+
+        public Class1(string textData, DateTime dateTimeData, double doubleData, Class2 class2, Class3 class3)
+        {
+            TextData = textData;
+            DateTimeData = dateTimeData;
+            DoubleData = doubleData;
+            Class2 = class2;
+            Class3 = class3;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("TextData", TextData);
+            info.AddValue("DateTimeData", DateTimeData);
+            info.AddValue("DoubleData", DoubleData);
+            info.AddValue("Class2", Class2);
+            info.AddValue("Class3", Class3);
+        }
+    }
+}
---
+++ Task_2/ConsoleSerializer/DataModel/Class3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace ConsoleSerializer.DataModel
+{
+    [Serializable]
+    [JsonObject]
+    public class Class3 : ISerializable
+    {
+        public string TextData { get; set; }
+        public DateTime DateTimeData { get; set; }
+        public long LongData { get; set; }
+        public Class1 Class1 { get; set; }
+        public Class2 Class2 { get; set; }
+
+        public Class3(string textData, DateTime dateTimeData, long longData, Class1 class1, Class2 class2)
+        {
+            TextData = textData;
+            DateTimeData = dateTimeData;
+            LongData = longData;
+            Class1 = class1;
+            Class2 = class2;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("TextData", TextData);
+            info.AddValue("DateTimeData", DateTimeData);
+            info.AddValue("LongData", LongData);
+            info.AddValue("Class1", Class1);
+            info.AddValue("Class2", Class2);
+        }
+    }
+}
