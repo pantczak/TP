06eea65a9dc8c699f347bbdaa311edec07ca2e48
WitoldPietrzak <witekpietrzak35@gmail.com>
2020-12-05 04:32:31 +0100

    Added Tests for object of class1 type containing references to class2 and class3 type objects

--- Task_2/ConsoleSerializerTests/JsonSerializerTest.cs
+++ Task_2/ConsoleSerializerTests/JsonSerializerTest.cs
@@ -37,5 +37,37 @@ namespace ConsoleSerializerTests
             ISerializable deserialized = JsonSerializer.Deserialize<Class1>(fileName);
             Assert.AreEqual(true,Equals(objectToSerialize, deserialized));
         }
+
+        [TestMethod]
+        public void Class1With2and3TestWrite()
+        {
+            Class2 class2 = new Class2("Text", DateTime.Now, 17, null, null);
+            Class3 class3 = new Class3("Text", DateTime.Now, 192834, null, null);
+            ISerializable objectToSerialize = new Class1("Text", DateTime.Now, 3.012, class2, class3);
+            JsonSerializer serializer = new JsonSerializer();
+            const string fileName = "testJson.json";
+            JsonSerializer.Serialize(objectToSerialize, fileName);
+            FileInfo info = new FileInfo(fileName);
+            Assert.IsTrue(info.Exists);
+            Assert.IsTrue(info.Length >= 100);
+            string fileContent = File.ReadAllText(fileName, Encoding.UTF8);
+            Debug.Write(fileContent);
+            File.Delete(fileName);
+        }
+
+        [TestMethod]
+        public void Class1With2and3TestRead()
+        {
+            Class2 class2 = new Class2("Text", DateTime.Now, 17, null, null);
+            Class3 class3 = new Class3("Text", DateTime.Now, 192834, null, null);
+            ISerializable objectToSerialize = new Class1("Text", DateTime.Now, 3.012, class2, class3);
+            JsonSerializer serializer = new JsonSerializer();
+            const string fileName = "testJson.json";
+            JsonSerializer.Serialize(objectToSerialize, fileName);
+            ISerializable deserialized = JsonSerializer.Deserialize<Class1>(fileName);
+            Assert.AreEqual(true, Equals(objectToSerialize, deserialized));
+        }
+
+
     }
 }
