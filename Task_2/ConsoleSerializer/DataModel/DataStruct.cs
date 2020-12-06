6cc1043ece2462420e02bffc0710b2f650ee3b4b
WitoldPietrzak <witekpietrzak35@gmail.com>
2020-12-06 08:54:57 +0100

    Added DataStruct struct to store Serialized objects attributes

---
+++ Task_2/ConsoleSerializer/DataModel/DataStruct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSerializer.DataModel
+{
+    struct DataStruct
+    {
+        public DataStruct(string className,string name,string value)
+        {
+            this.className = className;
+            this.name = name;
+            this.value = value;
+        }
+        public string className;
+        public string name;
+        public string value;
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(className+ " "+name+" "+value);
+            return stringBuilder.ToString();
+        }
+    }
+}
