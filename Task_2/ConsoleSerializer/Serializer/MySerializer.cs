d932ae7c194865c3e93b7c0b2a64e3abb56e8b20
pantczak <piotrmateuszantczak@protonmail.com>
2020-12-06 13:55:31 +0100

    Modified DataClasses added Program

--- Task_2/ConsoleSerializer/Serializer/MySerializer.cs
+++ Task_2/ConsoleSerializer/Serializer/MySerializer.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Text;
-using System.Xml;
-using System.Xml.Linq;
 using ConsoleSerializer.DataModel;
-using Newtonsoft.Json;
 
 namespace ConsoleSerializer.Serializer
 {
@@ -16,6 +13,7 @@ namespace ConsoleSerializer.Serializer
         List<ObjStruct> _objects = new List<ObjStruct>();
         List<Object> _sobjects = new List<Object>();
         Stream serializationStream;
+
         public override object Deserialize(Stream serializationStream)
         {
             throw new NotImplementedException();
@@ -28,53 +26,52 @@ namespace ConsoleSerializer.Serializer
             SerializationInfo _info = new SerializationInfo(graph.GetType(), new FormatterConverter());
             StreamingContext _context = new StreamingContext(StreamingContextStates.File);
             _data.GetObjectData(_info, _context);
-             foreach (SerializationEntry _item in _info)
-             {
-                 this.WriteMember(_item.Name, _item.Value);
+            foreach (SerializationEntry _item in _info)
+            {
+                this.WriteMember(_item.Name, _item.Value);
             }
+
             StringBuilder fileContent = new StringBuilder("[");
 
-            ObjStruct graphStruct=new ObjStruct(graph);
+            ObjStruct graphStruct = new ObjStruct(graph);
             foreach (ObjStruct ost in _objects)
             {
                 if (ost.value == graph)
                 {
-                 graphStruct =ost;
+                    graphStruct = ost;
                     break;
                 }
             }
+
             if (!_objects.Contains(graphStruct))
             {
                 _objects.Add(graphStruct);
             }
+
             fileContent.Append(graphStruct.guid.ToString() + "\n");
             foreach (DataStruct dataStruct in _values)
             {
                 fileContent.Append(dataStruct.ToString() + "\n");
             }
+
             fileContent.Append("]\n");
-            using (StreamWriter writer = new StreamWriter(serializationStream, Encoding.UTF8,32, true))
+            using (StreamWriter writer = new StreamWriter(serializationStream, Encoding.UTF8, 32, true))
             {
                 writer.Write(fileContent.ToString());
             }
+
             fileContent.Clear();
             _values.Clear();
             _sobjects.Add(graph);
             foreach (ObjStruct objStruct in _objects)
             {
-                if(!_sobjects.Contains(objStruct.value))
-                    {
-                    this.Serialize(serializationStream,objStruct.value);
-
+                if (!_sobjects.Contains(objStruct.value))
+                {
+                    this.Serialize(serializationStream, objStruct.value);
                 }
             }
+
             serializationStream.Close();
-
-
-
-
-
-
         }
 
         public override ISurrogateSelector SurrogateSelector { get; set; }
@@ -135,18 +132,19 @@ namespace ConsoleSerializer.Serializer
         {
             if (obj == null)
             {
-                _values.Add(new DataStruct("null", name,"null"));
+                _values.Add(new DataStruct("null", name, "null"));
                 return;
             }
+
             if (memberType == typeof(String))
             {
-                _values.Add(new DataStruct(memberType.Name, name,(String)obj));
+                _values.Add(new DataStruct(memberType.Name, name, (String) obj));
                 return;
-
             }
+
             bool isOnList = false;
-            Guid onListGuid= new Guid();
-            foreach(ObjStruct objStruct in _objects)
+            Guid onListGuid = new Guid();
+            foreach (ObjStruct objStruct in _objects)
             {
                 if (objStruct.value == obj)
                 {
@@ -154,16 +152,16 @@ namespace ConsoleSerializer.Serializer
                     onListGuid = objStruct.guid;
                 }
             }
-            if(!isOnList)
+
+            if (!isOnList)
             {
                 ObjStruct objStruct = new ObjStruct(obj);
                 _objects.Add(objStruct);
                 onListGuid = objStruct.guid;
             }
-                _values.Add(new DataStruct(memberType.Name, name, onListGuid.ToString()));
-                return;
-            
 
+            _values.Add(new DataStruct(memberType.Name, name, onListGuid.ToString()));
+            return;
         }
 
         protected override void WriteSByte(sbyte val, string name)
