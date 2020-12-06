f9829d8168784f2e5acec75a61fa8fdcb6d92465
WitoldPietrzak <witekpietrzak35@gmail.com>
2020-12-06 08:55:49 +0100

    Added ObjStruct structure holding objects for Serialization

---
+++ Task_2/ConsoleSerializer/DataModel/ObjStruct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSerializer.DataModel
+{
+    struct ObjStruct
+    {
+        public ObjStruct(Object value)
+        {
+            this.value = value;
+            this.guid = Guid.NewGuid();
+        }
+        public Object value;
+        public Guid guid;
+
+    }
+}
