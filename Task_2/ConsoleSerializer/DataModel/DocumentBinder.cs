19f2e80d605339e84303126915ebf8e849038732
pantczak <piotrmateuszantczak@protonmail.com>
2020-12-08 14:32:00 +0100

    added Data classes, removed references to Task1

---
+++ Task_2/ConsoleSerializer/DataModel/DocumentBinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace ConsoleSerializer.DataModel
+{
+    [Serializable]
+    [JsonObject]
+    public class DocumentBinder : ISerializable
+    {
+        private LinkedList<Document> _documents = new  LinkedList<Document>();
+
+        public DocumentBinder()
+        {
+        }
+
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Documents",_documents);
+        }
+
+        public DocumentBinder(SerializationInfo info, StreamingContext context)
+        {
+            _documents = (LinkedList<Document>) info.GetValue("Documents",typeof(LinkedList<Document>));
+        }
+    }
+}
