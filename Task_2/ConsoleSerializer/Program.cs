8d26f290792f937c4e5d23e65b4640474ea7d367
pantczak <piotrmateuszantczak@protonmail.com>
2020-12-08 17:32:10 +0100

    Update Program.cs

--- Task_2/ConsoleSerializer/Program.cs
+++ Task_2/ConsoleSerializer/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using ConsoleSerializer.DataModel;
+using ConsoleSerializer.Serializer;
 
 namespace ConsoleSerializer
 {
@@ -9,7 +13,7 @@ namespace ConsoleSerializer
         {
             Console.WriteLine("Available operations: ");
             Console.WriteLine(
-                "[1] Serialize graph \n[2] Deserialize graph \n[3] Serialize graph to JSON\n[4] Deserialize graph from JSON\n[5] Exit");
+                "[1] Serialize graph \n[2] Deserialize graph \n[3] Serialize graph to JSON\n[4] Deserialize graph from JSON\n[5] Serialize List to JSON\n[6] Deserialize List from JSON\n[7] Exit");
             int choice = 0;
 
             Class1 class1 = new Class1("Text", DateTime.Now, 5.5);
@@ -25,8 +29,13 @@ namespace ConsoleSerializer
             class2.Class1 = class1;
             class2.Class3 = class3;
 
+            DocumentBinder binder = new DocumentBinder(new ObservableCollection<Document>
+            {
+                new Document(123, "342"),
+                new Document(23, "sdsd")
+            }, new string[] {"a1", "a2", "a3"});
 
-            while (choice != 5)
+            while (choice != 7)
             {
                 choice = Console.Read() - '0';
                 switch (choice)
@@ -36,12 +45,38 @@ namespace ConsoleSerializer
                     case 2:
                         break;
                     case 3:
-                        //TODO add JSON serialization
+                        using (FileStream fileStream = new FileStream("serializationGraph.json", FileMode.Create))
+                        {
+                            JsonSerializer.Serialize(fileStream, class1);
+                            Console.WriteLine("Object serialized");
+                            Console.WriteLine("Path: " + Directory.GetCurrentDirectory());
+                        }
                         break;
                     case 4:
-                        //TODO add JSON deserialization
+                        using (FileStream fileStream = new FileStream("serializationGraph.json", FileMode.Open))
+                        {
+                            Class1 deserialize = JsonSerializer.Deserialize<Class1>(fileStream);
+                            Console.WriteLine("Object deserialized");
+
+                        }
                         break;
                     case 5:
+                        using (FileStream fileStream = new FileStream("serializationList.json", FileMode.Create))
+                        {
+                            JsonSerializer.Serialize(fileStream, binder);
+                            Console.WriteLine("Object serialized");
+                            Console.WriteLine("Path: " + Directory.GetCurrentDirectory());
+                        }
+                        break;
+                    case 6:
+                        using (FileStream fileStream = new FileStream("serializationList.json", FileMode.Open))
+                        {
+                            DocumentBinder deserialize = JsonSerializer.Deserialize<DocumentBinder>(fileStream);
+                            Console.WriteLine("Object deserialized");
+                        }
+                        break;
+                    case 7:
+                        Environment.Exit(0);
                         break;
                     default:
                         break;
