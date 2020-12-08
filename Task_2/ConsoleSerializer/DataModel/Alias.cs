d1d01897cc9b29921e62f83b7377a74101c2ee13
pantczak <piotrmateuszantczak@protonmail.com>
2020-12-08 20:19:54 +0100

    Changed string array to Alias array

---
+++ Task_2/ConsoleSerializer/DataModel/Alias.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ConsoleSerializer.DataModel
+{
+    [Serializable]
+    [JsonObject]
+    public class Alias
+    {
+        public string Name { get; set; }
+
+        public Alias(string name)
+        {
+            Name = name;
+        }
+
+        protected bool Equals(Alias other)
+        {
+            return Name == other.Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Alias) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Name != null ? Name.GetHashCode() : 0);
+        }
+    }
+}
