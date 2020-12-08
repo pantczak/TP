374bc840bc76984f8a6e8669f5b2741cca4fd8c0
WitoldPietrzak <witekpietrzak35@gmail.com>
2020-12-08 10:12:27 +0100

    Added tests for correct deserialization

--- Task_2/ConsoleSerializerTests/MySerializerTest.cs
+++ Task_2/ConsoleSerializerTests/MySerializerTest.cs
@@ -14,14 +14,13 @@ namespace ConsoleSerializerTests
         public void TestWrite()
         {
             Class1 class1 = new Class1("Text", DateTime.Now, 3.012, null, null);
-            Class2 class2 = new Class2("Text", DateTime.Now, 17, null, null);
-            Class3 class3 = new Class3("Text", DateTime.Now, 192834, null, null);
+            Class2 class2 = new Class2("Text", DateTime.Now, null, null);
+            Class3 class3 = new Class3("Text", DateTime.Now, null, null);
             class1.Class2 = class2;
             class1.Class3 = class3;
             class2.Class3 = class3;
             class3.Class1 = class1;
-            //class2.Class1 = objectToSerialize;
-            //class2.Class3 = class3;
+
 
             MySerializer serializer = new MySerializer();
             const string filePath = "testsCustom.txt";
@@ -29,12 +28,31 @@ namespace ConsoleSerializerTests
             Stream fileStream = new FileStream(filePath, FileMode.Create);
             serializer.Serialize( fileStream, class1);
             fileStream.Close();
-            //FileInfo info = new FileInfo(filePath);
-            //Assert.IsTrue(info.Exists);
-            //Assert.IsTrue(info.Length >= 100);
-            //string fileContent = File.ReadAllText(fileName, Encoding.UTF8);
-            //Debug.Write(fileContent);
-            //File.Delete(fileName);
+        }
+        [TestMethod]
+        public void TestRead()
+        {
+            Class1 class1 = new Class1("Text", DateTime.Now, 3.012, null, null);
+            Class2 class2 = new Class2("Text", DateTime.Now, null, null);
+            Class3 class3 = new Class3("Text", DateTime.Now, null, null);
+            class1.Class2 = class2;
+            class1.Class3 = class3;
+            class2.Class3 = class3;
+            class3.Class1 = class1;
+
+
+            MySerializer serializer = new MySerializer();
+            const string filePath = "testsCustom.txt";
+
+            Stream fileStream = new FileStream(filePath, FileMode.Create);
+            serializer.Serialize(fileStream, class1);
+            fileStream.Close();
+
+            fileStream = new FileStream(filePath, FileMode.Open);
+            Class1 obj = (Class1)serializer.Deserialize(fileStream);
+            fileStream.Close();
+            Assert.AreEqual(class1, obj);
+            Assert.AreEqual(true, Equals(class1, obj));
         }
     }
 }
