01e50ee065ebabd98989ea801edbe9ed2580d695
pantczak <piotrmateuszantczak@protonmail.com>
2020-12-28 12:06:56 +0100

    Added ORM for tables in Production schema

---
+++ Task3/Task3/ToolClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Task3.Database;
+
+namespace Task3
+{
+    public static class ToolClass
+    {
+        public static List<Product> GetProductsByName(string namePart)
+        {
+            DataClasses1DataContext productionDataContext = new DataClasses1DataContext();
+            List<Product> productsList = new List<Product>(from product in productionDataContext.Products
+                where product.Name.Contains(namePart)
+                select product);
+            return productsList;
+        }
+    }
+}
