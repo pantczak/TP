4dd4f1bd742b1e5225b4c9839f51320fb6ab5e9d
pantczak <piotrmateuszantczak@protonmail.com>
2020-12-28 17:51:03 +0100

    Added tests template

---
+++ Task3/Task3Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Task3Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task3Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ea7f1b40-befc-40b3-8069-476d5def8d92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Task3/Task3Tests/ToolClassTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Task3.Database;
+
+namespace Task3Tests
+{
+    [TestClass]
+    public class ToolClassTest
+    {
+        //TODO tests are probably wrong ( no DB conn )
+
+        [TestMethod]
+        public void GetProductsByName()
+        {
+            List<Product> listOfProducts = SqlToolClass.GetProductsByName("Decal");
+        }
+
+        [TestMethod]
+        public void GetProductsByVendorName()
+        {
+        }
+
+        [TestMethod]
+        public void GetProductNamesByVendorName()
+        {
+        }
+
+        [TestMethod]
+        public void GetProductVendorByProductName()
+        {
+        }
+
+        [TestMethod]
+        public void GetProductsWithNRecentReviews()
+        {
+        }
+
+        [TestMethod]
+        public void GetNRecentlyReviewedProducts()
+        {
+        }
+
+        [TestMethod]
+        public void GetTotalStandardCostByCategory()
+        {
+        }
+    }
+}
