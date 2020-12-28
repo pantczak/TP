f12df2d63015ca4f0725dc6670068ced6e15a993
pantczak <piotrmateuszantczak@protonmail.com>
2020-12-28 13:10:28 +0100

    added methods to ToolClass.cs

--- Task3/Task3/ToolClass.cs
+++ Task3/Task3/ToolClass.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using Task3.Database;
 
 namespace Task3
@@ -9,10 +10,67 @@ namespace Task3
         public static List<Product> GetProductsByName(string namePart)
         {
             DataClasses1DataContext productionDataContext = new DataClasses1DataContext();
-            List<Product> productsList = new List<Product>(from product in productionDataContext.Products
+            List<Product> productList = new List<Product>(from product in productionDataContext.Products
                 where product.Name.Contains(namePart)
                 select product);
-            return productsList;
+            return productList;
+        }
+
+        public static List<Product> GetProductsByVendorName(string vendorName)
+        {
+            DataClasses1DataContext productionDataContext = new DataClasses1DataContext();
+            List<Product> productList = new List<Product>(from product in productionDataContext.Products
+                join productVendor in productionDataContext.ProductVendors on product.ProductID equals
+                    productVendor.ProductID
+                where productVendor.Vendor.Name == vendorName
+                select product);
+            return productList;
+        }
+
+        public static List<string> GetProductNamesByVendorName(string vendorName)
+        {
+            DataClasses1DataContext productionDataContext = new DataClasses1DataContext();
+            List<string> productNameList = new List<string>(from product in productionDataContext.Products
+                join productVendor in productionDataContext.ProductVendors on product.ProductID equals
+                    productVendor.ProductID
+                where productVendor.Vendor.Name == vendorName
+                select product.Name);
+            return productNameList;
+        }
+
+        public static string GetProductVendorByProductName(string productName)
+        {
+            DataClasses1DataContext productionDataContext = new DataClasses1DataContext();
+            return (from product in productionDataContext.Products
+                join productVendor in productionDataContext.ProductVendors on product.ProductID equals productVendor
+                    .ProductID
+                where product.Name == productName
+                select productVendor.Vendor.Name).First();
+        }
+
+        public static List<Product> GetProductsWithNRecentReviews(int howManyReviews)
+        {
+            DataClasses1DataContext productionDataContext = new DataClasses1DataContext();
+            List<Product> productList = new List<Product>(from product in productionDataContext.Products
+                where product.ProductReviews.Count == howManyReviews
+                select product);
+            return productList;
+        }
+
+        public static List<Product> GetNRecentlyReviewedProducts(int howManyProducts)
+        {
+            DataClasses1DataContext productionDataContext = new DataClasses1DataContext();
+            List<Product> productList = new List<Product>((from review in productionDataContext.ProductReviews
+                orderby review.ReviewDate descending
+                group review.Product by review.ProductID into tuple
+                select tuple.First()).Take(howManyProducts));
+            return productList;
+        }
+
+        public static int GetTotalStandardCostByCategory(ProductCategory category)
+        {
+            //TODO
+            return 0;
         }
     }
 }
