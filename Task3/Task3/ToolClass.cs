9980ed39ea93cde4ee83ef553f2831d0f98a77c9
pantczak <piotrmateuszantczak@protonmail.com>
2020-12-29 11:42:35 +0100

    Added missing tests for ToolClass.cs

--- Task3/Task3/ToolClass.cs
+++ Task3/Task3/ToolClass.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Net.Sockets;
 using Task3.Database;
@@ -9,71 +10,98 @@ namespace Task3
     {
         public static List<Product> GetProductsByName(string namePart)
         {
-            DataClasses1DataContext productionDataContext = new DataClasses1DataContext();
-            List<Product> productList = new List<Product>(from product in productionDataContext.Products
-                where product.Name.Contains(namePart)
-                select product);
-            return productList;
+            using (DataClasses1DataContext context = new DataClasses1DataContext())
+            {
+                List<Product> productList = new List<Product>(from product in context.Products
+                    where product.Name.Contains(namePart)
+                    select product);
+                return productList;
+            }
         }
 
         public static List<Product> GetProductsByVendorName(string vendorName)
         {
-            DataClasses1DataContext productionDataContext = new DataClasses1DataContext();
-            List<Product> productList = new List<Product>(from product in productionDataContext.Products
-                join productVendor in productionDataContext.ProductVendors on product.ProductID equals
-                    productVendor.ProductID
-                where productVendor.Vendor.Name == vendorName
-                select product);
-            return productList;
+            using (DataClasses1DataContext context = new DataClasses1DataContext())
+            {
+                List<Product> productList = new List<Product>(from product in context.Products
+                    join productVendor in context.ProductVendors on product.ProductID equals
+                        productVendor.ProductID
+                    where productVendor.Vendor.Name == vendorName
+                    select product);
+                return productList;
+            }
         }
 
         public static List<string> GetProductNamesByVendorName(string vendorName)
         {
-            DataClasses1DataContext productionDataContext = new DataClasses1DataContext();
-            List<string> productNameList = new List<string>(from product in productionDataContext.Products
-                join productVendor in productionDataContext.ProductVendors on product.ProductID equals
-                    productVendor.ProductID
-                where productVendor.Vendor.Name == vendorName
-                select product.Name);
-            return productNameList;
+            using (DataClasses1DataContext context = new DataClasses1DataContext())
+            {
+                List<string> productNameList = new List<string>(from product in context.Products
+                    join productVendor in context.ProductVendors on product.ProductID equals
+                        productVendor.ProductID
+                    where productVendor.Vendor.Name == vendorName
+                    select product.Name);
+                return productNameList;
+            }
         }
 
         public static string GetProductVendorByProductName(string productName)
         {
-            DataClasses1DataContext productionDataContext = new DataClasses1DataContext();
-            return (from product in productionDataContext.Products
-                join productVendor in productionDataContext.ProductVendors on product.ProductID equals productVendor
-                    .ProductID
-                where product.Name == productName
-                select productVendor.Vendor.Name).First();
+            using (DataClasses1DataContext context = new DataClasses1DataContext())
+            {
+                return (from product in context.Products
+                    join productVendor in context.ProductVendors on product.ProductID equals productVendor
+                        .ProductID
+                    where product.Name == productName
+                    select productVendor.Vendor.Name).First();
+            }
         }
 
         public static List<Product> GetProductsWithNRecentReviews(int howManyReviews)
         {
-            DataClasses1DataContext productionDataContext = new DataClasses1DataContext();
-            List<Product> productList = new List<Product>(from product in productionDataContext.Products
-                where product.ProductReviews.Count == howManyReviews
-                select product);
-            return productList;
+            using (DataClasses1DataContext context = new DataClasses1DataContext())
+            {
+                List<Product> productList = new List<Product>(from product in context.Products
+                    where product.ProductReviews.Count == howManyReviews
+                    select product);
+                return productList;
+            }
         }
 
         public static List<Product> GetNRecentlyReviewedProducts(int howManyProducts)
         {
-            DataClasses1DataContext productionDataContext = new DataClasses1DataContext();
-            List<Product> productList = new List<Product>((from review in productionDataContext.ProductReviews
-                orderby review.ReviewDate descending
-                group review.Product by review.ProductID
-                into tuple
-                select tuple.First()).Take(howManyProducts));
-            return productList;
+            using (DataClasses1DataContext context = new DataClasses1DataContext())
+            {
+                List<Product> productList = new List<Product>((from review in context.ProductReviews
+                    orderby review.ReviewDate descending
+                    group review.Product by review.ProductID
+                    into tuple
+                    select tuple.First()).Take(howManyProducts));
+                return productList;
+            }
         }
+
+        public static List<Product> GetNProductsFromCategory(string categoryName, int n)
+        {
+            using (DataClasses1DataContext context = new DataClasses1DataContext())
+            {
+                List<Product> productList = new List<Product>((from product in context.Products
+                    where product.ProductSubcategory.ProductCategory.Name.Equals(categoryName)
+                    select product).Take(n).ToList());
+
+                return productList;
+            }
+        }
+
 
         public static int GetTotalStandardCostByCategory(ProductCategory category)
         {
-            DataClasses1DataContext productionDataContext = new DataClasses1DataContext();
-            return (int) (from product in productionDataContext.Products
-                where product.ProductSubcategory.ProductCategory.Name == category.Name
-                select product.StandardCost).Sum();
+            using (DataClasses1DataContext context = new DataClasses1DataContext())
+            {
+                return (int) (from product in context.Products
+                    where product.ProductSubcategory.ProductCategory.Name == category.Name
+                    select product.StandardCost).Sum();
+            }
         }
     }
 }
