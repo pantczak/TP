fee68e9625cca037ef2fc2b694205ac4cb70ae4c
pantczak <piotrmateuszantczak@protonmail.com>
2020-12-29 12:08:10 +0100

    Added ExtensionToolClass

---
+++ Task3/Task3Tests/ExtensionToolClassTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task3Tests
+{
+    [TestClass]
+    public class ExtensionToolClassTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            //TODO fill tests
+        }
+    }
+}
