283662483ae00b3245661e965849cec2355249fa
pantczak <piotrmateuszantczak@protonmail.com>
2020-12-29 11:42:35 +0100

    Added missing tests for ToolClass.cs

--- Task3/Task3Tests/ToolClassTest.cs
+++ Task3/Task3Tests/ToolClassTest.cs
@@ -14,36 +14,68 @@ namespace Task3Tests
         [TestMethod]
         public void GetProductsByName()
         {
+            List<Product> list = ToolClass.GetProductsByName("Hex Nut");
+
+            Assert.AreEqual(39, list.Count);
+
+            foreach (Product product in list)
+            {
+                Assert.IsTrue(product.Name.Contains("Hex Nut"));
+            }
         }
 
         [TestMethod]
         public void GetProductsByVendorName()
         {
+            List<Product> list = ToolClass.GetProductsByVendorName("Trikes, Inc.");
+
+            Assert.AreEqual(2, list.Count);
         }
 
         [TestMethod]
         public void GetProductNamesByVendorName()
         {
+            List<string> list = ToolClass.GetProductNamesByVendorName("Trikes, Inc.");
+            Assert.AreEqual("Mountain Tire Tube", list[0]);
+            Assert.AreEqual("HL Mountain Tire", list[1]);
+            Assert.AreEqual(2, list.Count);
         }
 
         [TestMethod]
         public void GetProductVendorByProductName()
         {
+            string name = ToolClass.GetProductVendorByProductName("Bearing Ball");
+
+            Assert.AreEqual("Wood Fitness", name);
         }
 
         [TestMethod]
         public void GetProductsWithNRecentReviews()
         {
+            List<Product> list = ToolClass.GetProductsWithNRecentReviews(2);
+            Assert.AreEqual(1, list.Count);
         }
 
         [TestMethod]
         public void GetNRecentlyReviewedProducts()
         {
+            List<Product> list = ToolClass.GetNRecentlyReviewedProducts(2);
+            Assert.AreEqual(2, list.Count);
+        }
+
+        [TestMethod]
+        public void NProductsFromCategory()
+        {
+            List<Product> list = ToolClass.GetNProductsFromCategory("Bikes", 3);
+
+            Assert.AreEqual(3, list.Count);
         }
 
         [TestMethod]
         public void GetTotalStandardCostByCategory()
         {
+            double totalCost = ToolClass.GetTotalStandardCostByCategory(new ProductCategory {Name = "Bikes"});
+            Assert.AreEqual( 92092,  totalCost);
         }
     }
 }
