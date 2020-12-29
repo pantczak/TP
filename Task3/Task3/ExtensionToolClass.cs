583b150a8fe712036693c29fbac72718d87f3946
pantczak <piotrmateuszantczak@protonmail.com>
2020-12-29 12:08:10 +0100

    Added ExtensionToolClass

---
+++ Task3/Task3/ExtensionToolClass.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Task3.Database;
+
+namespace Task3
+{
+    public static class ExtensionToolClass
+    {
+        public static List<Product> GetProductsWithNoCategory(this List<Product> productsList)
+        {
+            List<Product> productsWithNoCategoryList = (from product in productsList
+                where product.ProductSubcategory == null
+                select product).ToList();
+
+            return productsWithNoCategoryList;
+        }
+
+        public static List<Product> GetProductsWithNoCategoryImperative(this List<Product> productsList)
+        {
+            return productsList.Where(prod => prod.ProductSubcategory == null).ToList();
+        }
+
+
+        public static List<Product> GetProductsAsPage(this List<Product> productsList, int pageNumber,
+            int productsPerPage)
+        {
+            List<Product> productsPage = new List<Product>(from product in productsList
+                select product).Skip(productsPerPage * (pageNumber - 1)).Take(productsPerPage).ToList();
+            return productsPage;
+        }
+
+        public static List<Product> GetProductsAsPageImperative(this List<Product> productsList, int pageNumber,
+            int productsPerPage)
+        {
+            List<Product> productsPage = new List<Product>(productsList.Skip(productsPerPage * (pageNumber - 1))
+                .Take(productsPerPage).ToList());
+
+            return productsPage;
+        }
+
+        //TODO ADD STRING RETURN 
+    }
+}
