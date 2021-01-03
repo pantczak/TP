4545dadd41337295385862e8883fe4a4f9545cb4
WitoldPietrzak <witekpietrzak35@gmail.com>
2021-01-04 00:46:55 +0100

    Added constructor to MyProduct class

--- Task3/Task3/MyProduct.cs
+++ Task3/Task3/MyProduct.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task3.Database;
 
 namespace Task3
 {
-    class MyProduct
+    public class MyProduct : Product
     {
     }
 }
