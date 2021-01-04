0b239d0fefb4a7cff58a4065c406a2d4fdb17047
WitoldPietrzak <witekpietrzak35@gmail.com>
2021-01-04 02:55:30 +0100

    Added constructor to MyProductDataContext class

--- Task3/Task3/MyProductDataContext.cs
+++ Task3/Task3/MyProductDataContext.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task3.Database;
 
 namespace Task3
 {
     class MyProductDataContext
     {
+        public static List<MyProduct> MyProducts { get; private set; }
+
+
+        public MyProductDataContext(List<Product> productsList)
+        {
+            var i = 0;
+
+            MyProducts = productsList.Select(product => new MyProduct(product)).ToList();
+        }
     }
 }
