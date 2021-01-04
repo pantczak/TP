4c158b0a165611e75d8246ccb21343ff96f4bf73
WitoldPietrzak <witekpietrzak35@gmail.com>
2021-01-04 18:12:00 +0100

    Added methods to MyProductTools class, modified MyProductDataContext class

--- Task3/Task3/MyProductTools.cs
+++ Task3/Task3/MyProductTools.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task3.Database;
 
 namespace Task3
 {
     class MyProductTools
     {
+        public static List<MyProduct> GetProductsByName(string namePart)
+        {
+                List<MyProduct> productList = new List<MyProduct>(from product in MyProductDataContext.MyProducts
+                                                              where product.Name.Contains(namePart)
+                                                              select product);
+                return productList;
+
+        }
+        public static List<MyProduct> GetProductsWithNRecentReviews(int howManyReviews)
+        {
+
+                List<MyProduct> productList = new List<MyProduct>(from product in MyProductDataContext.MyProducts
+                                                              where product.ProductReviews.Count == howManyReviews
+                                                              select product);
+                return productList;
+        }
+
+        public static List<MyProduct> GetNProductsFromCategory(string categoryName, int n)
+        {
+                List<MyProduct> productList = new List<MyProduct>((from product in MyProductDataContext.MyProducts
+                                                               where product.ProductSubcategory.ProductCategory.Name.Equals(categoryName)
+                                                               select product).Take(n).ToList());
+
+                return productList;
+        }
+    }
+        }
     }
 }
