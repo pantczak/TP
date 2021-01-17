ea102caab013a0ed38499abb842aff582e561dd5
pantczak <piotrmateuszantczak@protonmail.com>
2021-01-17 17:48:38 +0100

    Added ViewModel project

--- Task4/Task4GUIModel/ProperProductCategoryModel.cs
+++ Task4/Task4GUIModel/ProperProductCategoryModel.cs
@@ -1,9 +1,46 @@
-﻿using Task4Service.ServiceClasses;
+﻿using System;
+using System.Collections.Generic;
+using Task4Service.ClassWrapper;
+using Task4Service.ServiceClasses;
 
 namespace Task4GUIModel
 {
     public class ProperProductCategoryModel : IModel
     {
-        private IDataRepository
+        private readonly IDataRepository _repository;
+
+        public ProperProductCategoryModel()
+        {
+            _repository = new DataRepository();
+        }
+
+        public void AddProductCategory(int productCategoryId, string name,
+            DateTime modifiedDate)
+        {
+            _repository.CreateProduct(
+                ProductCategoryConverter.NewCategoryPlaceholder(productCategoryId, name, modifiedDate));
+        }
+
+        public void DeleteProductCategory(int productCategoryId)
+        {
+            _repository.DeleteProduct(productCategoryId);
+        }
+
+        public List<ProductCategoryPlaceholder> GetAllProductCategories()
+        {
+            return (List<ProductCategoryPlaceholder>) _repository.ReadAllProducts();
+        }
+
+        public void UpdateProductCategory(int productCategoryId, string name,
+            DateTime modifiedDate)
+        {
+            _repository.UpdateProduct(
+                ProductCategoryConverter.NewCategoryPlaceholder(productCategoryId, name, modifiedDate));
+        }
+
+        public ProductCategoryPlaceholder GetProductCategory(int productCategoryId)
+        {
+            return _repository.ReadProduct(productCategoryId);
+        }
     }
 }
