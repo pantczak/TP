101e5215f38cff5c86ded1768acfad5eea41b207
pantczak <piotrmateuszantczak@protonmail.com>
2021-01-23 15:42:17 +0100

    Implemented required interfaces, minor fixes

---
+++ Task4/Task4GUIViewModel/IDetailInfoWindow.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace Task4GUIViewModel
+{
+    public interface IDetailInfoWindow
+    {
+        void ShowInfoWindow<T>(T viewModel) where T : INotifyPropertyChanged;
+    }
+}
