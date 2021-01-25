29c25ec45014f7a488d0930332a297f0d4209bed
WitoldPietrzak <witekpietrzak35@gmail.com>
2021-01-25 18:08:16 +0100

    Created xamml view base

--- Task4/Task4GUI/MainWindow.xaml.cs
+++ Task4/Task4GUI/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Task4GUI
         {
             InitializeComponent();
         }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
